82daa72495d667cc9008ca6d44be6ec892f91fb1
ram123555 <ramkrishnatah2001@gmail.com>
2026-06-22 16:29:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://ramkrishnatah2020_db_user:3fmwhqnErqCu3fwr@cluster0.v23dpwf.mongodb.net/doctorApp");
+  await mongoose.connect("mongodb+srv://ramkrishnatah2020_db_user:GC7YgSeHXqUVQBx7@cluster0.v23dpwf.mongodb.net/doctorApp");
   console.log("MongoDB Connected");
 };
 
