8b71804ea6cba4a406a91e67ca3bce1e58d9856b
ram123555 <ramkrishnatah2001@gmail.com>
2026-06-23 21:09:10 +0530

    Update contact.routes.js

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -73,7 +73,7 @@ router.post("/", async (req, res) => {
 
     await resend.emails.send({
 
-      from: "HealthCare <anything>@ontaefau.resend.app",
+      from: "HealthCare <anything>@ontaefau.resend.app>",
       to: email,
       subject: "Thank you for contacting HealthCare",
 
